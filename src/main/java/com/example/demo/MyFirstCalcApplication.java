5f6b80d59ddeb06f5a6cf00017bb92289604d5e5
CRTXScyNet <CRTXScyNet@gmail.com>
2024-05-05 20:13:10 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/MyFirstCalcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstCalcApplication.class, args);
+	}
+
+}
